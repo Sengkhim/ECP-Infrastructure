fcfc48b51065299f21c64f86c690f6578553d0b6
Sengkhim <75316691+Sengkhim@users.noreply.github.com>
2025-05-05 17:33:20 +0700

    Push to nuget

--- ECPLibrary.Tests/obj/Debug/net8.0/ECPLibrary.Tests.AssemblyInfo.cs
+++ ECPLibrary.Tests/obj/Debug/net8.0/ECPLibrary.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECPLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cedc3f881697ee8e9b577732bbf676f17197142b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474cbafbdf4cc8c355804c92af4cd6a7a187bef5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECPLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECPLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ECPLibrary/README.md
+++ ECPLibrary/README.md
@@ -1,5 +1,5 @@
 ﻿1- dotnet pack
-2- dotnet nuget push bin/Release/Sengkhim.ECP.Infrastructure.1.1.1.nupkg --api-key oy2cuoymnpm52jotkpjaceptvqntgo2enpjxquqamimgeq --source https://api.nuget.org/v3/index.json
+2- dotnet nuget push bin/Release/SK.ECP.Infrastructure.1.0.6.nupkg --api-key oy2esfogp2bbhh2ako4avdr2hepqrnfenjtx3dhxivdh3a --source https://api.nuget.org/v3/index.json
 
 Usage: Database connection
 builder.Services
